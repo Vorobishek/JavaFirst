4549c1898139c843325ea59f1819ccf46951cd4b
Vorobishek <vorobishek@list.ru>
2021-10-03 10:26:20 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentSumAccount = 2_000_000_000;
+        int addSum = 500_000_000;
+        int totalSum = currentSumAccount + addSum;
+        System.out.println(totalSum);
+    }
+}
